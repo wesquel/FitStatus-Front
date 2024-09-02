16dfc6b2507c4bec8c3be64324be2bb04ac1ed64
Weslley Addson <wesquel@hotmail.com>
2024-09-01 21:05:06 -0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CustomInputComponent } from "./components/custom-input/custom-input.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { NavbarComponent } from './components/navBar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CustomInputComponent,
+    FormsModule,
+    LoginComponent,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FitStatus-Front';
+
 }
