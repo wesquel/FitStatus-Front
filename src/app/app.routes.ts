1187de84c826da547f29d055a71baae4138a7d16
Weslley Addson <wesquel@hotmail.com>
2024-09-01 21:04:30 -0300

    create router added provide to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navBar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'nav', component: NavbarComponent}
+];
