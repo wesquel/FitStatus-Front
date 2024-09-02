e76cf6ffc780ce2f24dda5551428ac7eb5bd554d
Weslley Addson <wesquel@hotmail.com>
2024-09-01 21:03:45 -0300

    create login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomInputComponent } from "../../components/custom-input/custom-input.component";
+import { AuthService } from '../../service/auth.service';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CustomInputComponent,
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  formGroup: FormGroup;
+
+  http = inject(HttpClient)
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.formGroup = this.fb.group({
+      name: ['init value', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+      this.formGroup.reset({name: 'new value'})
+  }
+
+  onSubmit(form: NgForm){
+
+    const loginData = {usernameOrEmail: form.value.username, password: form.value.password}
+
+    // Realização de autenticação
+    this.authService.login(loginData).subscribe(
+      success => {
+        if (success) {
+          alert("sucesso no login")
+        } else {
+          alert("falha no login")
+          this.authService.logout()
+        }
+      }, error => {
+      alert("falha no login!");
+      this.authService.logout();
+    });
+
+  }
+}
